3e9f7b4bc98b8f91d35aaff620ab33a5c0bd8191
Stigus96 <stighelgebruset@msn.com>
2020-10-05 11:15:05 +0200

    added new main for server

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main {
+    //Default TCP port to use
+    private static final int DEFAULT_PORT = 1300;
+
+
+    public static void main(String[] args) {
+        //Start new server on a given port
+        int port = DEFAULT_PORT;
+        if(args.length > 0){
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid port specified: " + args[0]);
+            }
+        }
+        Server server = new Server();
+        server.start(port);
+    }
+
+
+
+}
