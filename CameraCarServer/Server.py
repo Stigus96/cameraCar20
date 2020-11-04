79605dd6c6c730de727929f8da94ea8950fd532a
Stigus96 <stighelgebruset@msn.com>
2020-11-04 16:01:20 +0100

    added threading to server

--- CameraCarServer/Server.py
+++ CameraCarServer/Server.py
@@ -1,53 +1,77 @@
 import socket
+
+from _thread import *
+import threading
 import sys
 
+print_lock = threading.Lock()
 
-class Server:
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    server_address = ('', 1300)
-    print('starting up on {} port {}'.format(*server_address))
-    serversocket.bind(server_address)
-    serversocket.listen(1)
+def threaded(connection):
+    while True:
+        try:
+            data = connection.recv(16)
+        except:
+            print("cannot receive data, client has probably forcibly closed connection")
+            break
+        if not data: break
+        cmd = data.decode("utf-8")
+        print('received ' + cmd)
+        cmd = cmd.strip()
+
+        if cmd == "START":
+            print('starting stuff')
+
+            connection.sendall(bytes("starting stuff", 'utf-8'))
+        if cmd == "VECTOR":
+            # incoming string on form "VECTOR speed angle"
+            cmdSplit = cmd.split(" ")
+            speed = cmdSplit[1]
+            angle = cmdSplit[2]
+            connection.sendall(bytes('vectors successfully received', 'utf-8'))
+        if cmd == "SENSOR_OFF":
+            # turn sensor off
+            print('turning sensor off')
+
+        if cmd == "SENSOR_ON":
+            # turn sensor on
+            print('turning sensor on')
+
+        else:
+            print('command not supported?')
+            connection.sendall(bytes('command not supported', 'utf-8'))
+
+    connection.close()
+
+
+def main():
+    port = 1300
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', 1300))
+    print('socket binded to port', port)
+
+    s.listen(5)
+    print("socket is listening")
 
     while True:
-        print('waiting for a connection')
-        connection, client_address = serversocket.accept()
-        try:
-            print('connection from', client_address)
+        # establish connection with client
+        c, addr = s.accept()
 
-            while True:
-                try:
-                    data = connection.recv(16)
-                except:
-                    print("cannot recive data, client has probably forcibly closed connection")
-                    break
-                if not data: break
-                cmd = data.decode("utf-8")
-                print('received ' + cmd)
-                cmd = cmd.strip()
+        # lock acquired by client
+        print_lock.acquire()
+        print('Connected to :', addr[0], ':', addr[1])
 
-                if cmd == "START":
-                    print('starting stuff')
+        # Start a new thread and return its identifier
+        start_new_thread(threaded, (c,))
+    s.close()
 
-                    connection.sendall(bytes("starting stuff", 'utf-8'))
-                if cmd == "VECTOR":
-                    # incoming string on form "VECTOR speed angle"
-                    cmdSplit = cmd.split(" ")
-                    speed = cmdSplit[1]
-                    angle = cmdSplit[2]
-                    connection.sendall(bytes('vectors successfully received', 'utf-8'))
-                if cmd == "SENSOR_OFF":
-                    #turn sensor off
-                    print('turning sensor off')
 
-                if cmd == "SENSOR_ON":
-                     #turn sensor on
-                    print('turning sensor on')
+if __name__ == '__main__':
+    main()
 
-                else:
-                    print('command not supported?')
-                    connection.sendall(bytes('command not supported', 'utf-8'))
+# serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        finally:
-            connection.close()
+# server_address = ('', 1300)
+# print('starting up on {} port {}'.format(*server_address))
+# serversocket.bind(server_address)
+# serversocket.listen(1)
