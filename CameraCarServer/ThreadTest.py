9e2560defa9e4cc0dce404b0de561f0825d032a5
Erlend N K <Erlend.n.knudsen@gmail.com>
2020-11-30 02:48:03 +0100

    Testing shared memory

---
+++ CameraCarServer/ThreadTest.py
@@ -0,0 +1,50 @@
+import threading
+import time
+from multiprocessing import shared_memory
+
+memory1 = shared_memory.SharedMemory(create=True, size=1)
+test = memory1.buf
+test[0] = "50"
+threads = []
+
+
+
+def shared_counter(shared_resource):
+    while True:
+        try:
+            shared_resource[0] = shared_resource[0] + 1
+            print("new value is", shared_resource[0])
+
+        except:
+            print("failed writing for some reason")
+        time.sleep(3)
+
+
+def shared_printer(shared_resource):
+    while True:
+        try:
+            print(shared_resource[0])
+        except:
+            print("failed reading for some reason")
+        time.sleep(1)
+
+
+def main():
+    count = 0
+
+    test1 = threading.Thread(target=shared_counter, args=(test,))
+    test2 = threading.Thread(target=shared_printer, args=(test,))
+    test1.start()
+    test2.start()
+
+    #    threads.append(test1)
+    #    threads.append(test2)
+
+    while True:
+        # do nothing
+        # count = count + 1
+        time.sleep(0.1)
+
+
+if __name__ == '__main__':
+    main()
